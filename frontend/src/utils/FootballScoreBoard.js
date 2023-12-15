4fa542022444382259566715a4fc7b05729a8986
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-12-15 01:32:01 +0100

    FootballScoreBoard class added into utils

---
+++ frontend/src/utils/FootballScoreBoard.js
@@ -0,0 +1,53 @@
+class FootballScoreBoard {
+  constructor() {
+    this.games = [];
+  }
+
+  startGame(homeTeam, awayTeam) {
+    const newGame = {
+      homeTeam,
+      awayTeam,
+      homeScore: 0,
+      awayScore: 0,
+      startTime: new Date(),
+    };
+    this.games.push(newGame);
+  }
+
+  findGame(homeTeam, awayTeam) {
+    return this.games.find(
+      (game) =>
+        game.homeTeam.toLowerCase() === homeTeam.toLowerCase() ||
+        game.awayTeam.toLowerCase() === awayTeam.toLowerCase() ||
+        game.homeTeam.toLowerCase() === awayTeam.toLowerCase() ||
+        game.awayTeam.toLowerCase() === homeTeam.toLowerCase()
+    );
+  }
+
+  updateScore(homeTeam, awayTeam, homeScore, awayScore) {
+    const game = this.findGame(homeTeam, awayTeam);
+    if (game) {
+      game.homeScore = homeScore;
+      game.awayScore = awayScore;
+    }
+  }
+
+  finishGame(homeTeam, awayTeam) {
+    this.games = this.games.filter(
+      (game) => game.homeTeam !== homeTeam || game.awayTeam !== awayTeam
+    );
+  }
+
+  getSummary() {
+    return this.games.slice().sort((a, b) => {
+      const totalScoreA = a.homeScore + a.awayScore;
+      const totalScoreB = b.homeScore + b.awayScore;
+      if (totalScoreA === totalScoreB) {
+        return b.startTime - a.startTime;
+      }
+      return totalScoreB - totalScoreA;
+    });
+  }
+}
+
+module.exports = FootballScoreBoard;
